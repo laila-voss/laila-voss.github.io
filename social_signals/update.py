82dbd7cfd535b1c2cd911492977fb6d4656533d1
Liam Newsam <liamcnewsam@gmail.com>
2025-08-04 14:29:15 -0700

    setup update python script

---
+++ social_signals/update.py
@@ -0,0 +1,4 @@
+# Run this every week, whereupon this file
+# will iterate over all entries in a master.csv of cumulative
+# data, compute what is wanted, and then write over the json
+# files in /reports
