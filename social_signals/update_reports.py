2c38e04178844c81ba29b84935fc91433df00af5
Liam Newsam <liamcnewsam@gmail.com>
2025-08-07 18:20:50 -0700

    more

--- social_signals/update_reports.py
+++ social_signals/update_reports.py
@@ -5,9 +5,11 @@
 
 
 import csv
+import datetime
+today = datetime.date.today()
 
-FILE_PATH = "data/craigslist_data.csv"
-
+CRAIGSLIST_DATA_FILE_PATH = "data/craigslist_data.csv"
+PROFILES_FILE_PATH = "data/profiles.csv"
 def read_csv_to_dict_list(file_path):
     data = []
     with open(file_path, mode='r', newline='', encoding='utf-8') as file:
@@ -15,6 +17,21 @@ def read_csv_to_dict_list(file_path):
         data = [row for row in reader]
     return data
 
+profiles = read_csv_to_dict_list(PROFILES_FILE_PATH)
+craigslist_data = read_csv_to_dict_list(CRAIGSLIST_DATA_FILE_PATH)
 
-for listing in read_csv_to_dict_list(FILE_PATH):
-    print(listing)
+test = craigslist_data[0]
+date_string = test["date"].split('T')[0]
+date = datetime.date.fromisoformat(date_string)
+
+print(today)
+print(date)
+print((today - date).days)
+'''
+for listing in craigslist_data:
+    date_string = listing["date"].split('T')[0]
+
+    date = datetime.date.fromiso(date_string)
+
+    print
+'''
